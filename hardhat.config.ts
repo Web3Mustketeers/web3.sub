38817019f785333e9ccfcf3ce7be67942089d033
Nasfame <laciferin@gmail.com>
2023-11-02 01:02:57 +0530

    default: network local
    Signed-off-by: Nasfame <laciferin@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "mumbai",
+  defaultNetwork: "local",
    networks: {
     // Add your network configurations here
     sepolia: {
