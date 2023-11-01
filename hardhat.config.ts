b64461f607571f28f02596b8bf094eb4fe08e4c2
Nasfame <laciferin@gmail.com>
2023-11-02 00:41:04 +0530

    wip: scripts fixes ci
    Signed-off-by: Nasfame <laciferin@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,14 +6,36 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  defaultNetwork: "mumbai",
    networks: {
     // Add your network configurations here
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.SEPOLIA_PRIVATE_KEY ?? ''],
+      accounts: [process.env.PRIVATE_KEY ?? ''],
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY ?? ''],
     },
   },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
 };
 
 export default config;
