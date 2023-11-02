9c8bd703dec005a34d13ff484abbddb39effe802
Nasfame <laciferin@gmail.com>
2023-11-03 00:54:57 +0530

    v1: formalize
    Signed-off-by: Nasfame <laciferin@gmail.com>

--- contracts/v1/v1.sol
+++ contracts/v1/v1.sol
@@ -1,32 +1,36 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
 contract V1 {
-    uint public unlockTime;
-    address payable public owner;
-
-    event NewSubscription();
-    event UnSubscribed();
+    event NewSubscription(address contractAddress, address wallet);
+    event UnSubscribed(address contractAddress, address wallet);
 
     constructor() {
         // TODO: make it upgradeable later
     }
 
     function subscribe(address contractAddress, address wallet) external {
-        emit NewSubscription();
+        // TODO: implement the logic
+        emit NewSubscription(contractAddress, wallet);
     }
 
     function unsubscribe(address contractAddress, address wallet) external {
-        emit UnSubscribed();
+      // TODO: implement the logic
+        emit UnSubscribed(contractAddress, wallet);
     }
 
     function verify(
         address contractAddress,
         address wallet
     ) external view returns (bool) {
+        require(contractAddress != address(0x0), "invalid contract address");
+        require(wallet != address(0x0), "invalid walletAddress");
+
+        // TODO: implement the logic
+
         return true;
     }
 }
