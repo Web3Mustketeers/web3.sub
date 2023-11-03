255b605065cba4968aa210b7f48856ab8c36fa6c
Hiro <laciferin@gmail.com>
2023-11-04 03:07:36 +0530

    feat: add web3Upgradeable Interface; revamp: web3subinterface (#21)
    * revamp: add UVx.sol, Vx.sol
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    * fixup: uv1.sol
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    * todo: vx
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    * uvx.sol
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    ---------
    
    Signed-off-by: Nasfame <laciferin@gmail.com>

--- contracts/Vx.sol
+++ contracts/Vx.sol
@@ -5,11 +5,11 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./web3sub.interface.sol";
 
+//TODO: Vx
 contract Vx is Web3Sub {
-    string public version = "v0.0.0"; //TODO: update the version string
+    string public version = string.concat("v", "x.0.0"); //TODO: update the version
 
     constructor() {
-        // TODO: make it upgradeable later
         string memory initMsg = string.concat(
             "init:",
             version,
@@ -18,25 +18,24 @@ contract Vx is Web3Sub {
         console.log(initMsg);
     }
 
-    function subscribe(address contractAddress, address wallet) external {
+    function subscribe(address wallet) external {
         // TODO: implement the logic
-        emit NewSubscription(contractAddress, wallet);
+        emit NewSubscription(wallet);
     }
 
-    function unsubscribe(address contractAddress, address wallet) external {
+    function unsubscribe(address wallet) external {
         // TODO: implement the logic
-        emit UnSubscribed(contractAddress, wallet);
+        emit UnSubscribed(wallet);
     }
 
-    function verify(
-        address contractAddress,
-        address wallet
-    ) external view returns (bool) {
-        require(contractAddress != address(0x0), "invalid contract address");
+    function verify(address wallet) external view returns (bool) {
+        require(msg.sender != address(0x0), "invalid contract address");
         require(wallet != address(0x0), "invalid walletAddress");
 
+        bool verified = false;
+
         // TODO: implement the logic
 
-        return true;
+        return verified;
     }
 }
--- contracts/web3sub.interface.sol
+++ contracts/web3sub.interface.sol
@@ -1,17 +1,14 @@
 pragma solidity ^0.8.9;
 
 interface Web3Sub {
-    event NewSubscription(address contractAddress, address wallet);
-    event UnSubscribed(address contractAddress, address wallet);
+    event NewSubscription(address wallet);
+    event UnSubscribed(address wallet);
 
-    function subscribe(address contractAddress, address wallet) external;
+    function subscribe(address wallet) external;
 
-    function unsubscribe(address contractAddress, address wallet) external;
+    function unsubscribe(address wallet) external;
 
-    function verify(
-        address contractAddress,
-        address wallet
-    ) external view returns (bool);
+    function verify(address wallet) external view returns (bool);
 
     function version() external view returns (string memory);
 }
