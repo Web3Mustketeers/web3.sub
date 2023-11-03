4784bbd8bbd01a7e983920c351ed0ade98e388f1
techyNonso <williamikeji@gmail.com>
2023-11-03 15:44:03 +0100

    functions implemented

--- contracts/v1/v1.sol
+++ contracts/v1/v1.sol
@@ -1,42 +1,49 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "../web3sub.interface.sol";
 
 contract V1 is Web3Sub {
-    string public version = "v1.0.0"; //TODO: update the version string
+    string public version; //TODO: update the version string
 
-    constructor() {
+    //subscriptions mappings
+    mapping(address => mapping(address => bool)) private Subscriptions;
+
+    constructor(string memory _version) {
         // TODO: make it upgradeable later
         string memory initMsg = string.concat(
             "init:",
             version,
             Strings.toString(block.timestamp)
         );
+        version = _version;
         console.log(initMsg);
     }
 
-    function subscribe(address contractAddress, address wallet) external {
-        // TODO: implement the logic
-        emit NewSubscription(contractAddress, wallet);
+    function subscribe( address wallet) external {
+        require(wallet != address(0x0), "invalid wallet address");
+        Subscriptions[msg.sender][wallet] = true;
+        emit NewSubscription(msg.sender, wallet);
     }
 
-    function unsubscribe(address contractAddress, address wallet) external {
-        // TODO: implement the logic
-        emit UnSubscribed(contractAddress, wallet);
+    function unsubscribe( address wallet) external {
+        require(wallet != address(0x0), "invalid wallet address");
+        Subscriptions[msg.sender][wallet] = false;
+        emit UnSubscribed(msg.sender, wallet);
     }
+
+
 
     function verify(
         address contractAddress,
         address wallet
-    ) external view returns (bool) {
+    ) external view returns (bool status) {
         require(contractAddress != address(0x0), "invalid contract address");
         require(wallet != address(0x0), "invalid walletAddress");
 
         // TODO: implement the logic
-
-        return true;
+        status =  Subscriptions[contractAddress][wallet];
     }
 }
--- contracts/web3sub.interface.sol
+++ contracts/web3sub.interface.sol
@@ -1,17 +1,19 @@
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 interface Web3Sub {
     event NewSubscription(address contractAddress, address wallet);
     event UnSubscribed(address contractAddress, address wallet);
 
-    function subscribe(address contractAddress, address wallet) external;
+    function subscribe( address wallet) external;
 
-    function unsubscribe(address contractAddress, address wallet) external;
+    function unsubscribe( address wallet) external;
 
     function verify(
         address contractAddress,
         address wallet
     ) external view returns (bool);
 
-    function version() external view returns (string memory);
+   //version is public so we do not need a function for it
+
 }
